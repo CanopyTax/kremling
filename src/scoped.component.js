462fafce29544c381b1762ed534d40ecc6787aa2
Joel Denning <joel.denning@canopytax.com>
2018-01-02 11:38:47 -0700

    Support for react@<16

--- src/scoped.component.js
+++ src/scoped.component.js
@@ -1,78 +1,111 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 const styleTags = {};
 let counter = 0;
 
-const reactVersion = Number(React.version.slice(0, React.version.indexOf('.')));
-const reactSupportsMultipleChildren = reactVersion >= 16;
+const reactSupportsReturningArrays = !!ReactDOM.createPortal;
 
 export class Scoped extends React.Component {
   static propTypes = {
     css: PropTypes.string.isRequired,
     namespace: PropTypes.string,
   }
-  static defaultNamespace = 'kremling';
+
+  static defaultNamespace = 'kremling'
+
   constructor(props) {
     super(props);
-    if (typeof props.css === 'string') {
-      if (props.css.indexOf('&') <= 0) {
-        console.warn(`Kremling's <Scoped css="..."> css prop should have the '&' character in it to scope the css classes`);
-      }
-      const existingDomEl = styleTags[props.css];
-      if (existingDomEl) {
-        this.styleRef = existingDomEl;
-        existingDomEl.kremlings++;
-        this.kremlingAttrName = this.styleRef.kremlingAttr;
-        this.kremlingAttrValue = this.styleRef.kremlingValue;
-      } else {
-        // The attribute for namespacing the css
-        this.kremlingAttrName = `data-${props.namespace || Scoped.defaultNamespace}`;
-        this.kremlingAttrValue = counter++;
-
-        // The css to append to the dom
-        const kremlingSelector = `[${this.kremlingAttrName}="${this.kremlingAttrValue}"]`;
-        const transformedCSS = props.css.replace(/& (.+){/g, (match, cssRule) => {
-          cssRule = cssRule.trim();
-          return `${kremlingSelector} ${cssRule}, ${kremlingSelector}${cssRule} {`;
-        });
-
-        // The dom element
-        const el = document.createElement('style');
-        el.setAttribute('type', 'text/css');
-        el.textContent = transformedCSS;
-        el.kremlings = 1;
-        el.kremlingAttr = kremlingSelector;
-        el.kremlingValue = this.kremlingAttrValue;
-        document.head.appendChild(el);
-        styleTags[props.css] = el;
-        this.styleRef = el;
-      }
-    }
+    this.state = this.newCssState(props);
   }
+
   render() {
     const kremlingChildren = React.Children.map(this.props.children, child => {
       if (React.isValidElement(child)) {
-        return React.cloneElement(child, {[this.kremlingAttrName]: this.kremlingAttrValue});
+        return React.cloneElement(child, {[this.state.kremlingAttrName]: this.state.kremlingAttrValue});
       } else {
         return child;
       }
     });
 
-    if (reactSupportsMultipleChildren) {
+    if (reactSupportsReturningArrays) {
       return kremlingChildren;
     } else {
       // React 15 or below
       if (kremlingChildren.length > 1) {
         throw new Error(`kremling's <Scoped /> component requires exactly one child element unless you are using react@>=16`);
-      } else {
+      } else if (kremlingChildren.length === 1) {
         return kremlingChildren[0];
+      } else {
+        return null;
       }
     }
   }
+
+  componentDidUpdate() {
+    if (this.state.css !== this.props.css) {
+      this.doneWithCss();
+      this.setState(this.newCssState(this.props))
+    }
+  }
+
   componentWillUnmount() {
-    if (--this.styleRef.kremlings === 0) {
-      this.styleRef.parentNode.removeChild(this.styleRef);
+    this.doneWithCss();
+  }
+
+  newCssState(props) {
+    if (typeof props.css !== 'string') {
+      return;
+    }
+
+    if (props.css.indexOf('&') <= 0) {
+      console.warn(`Kremling's <Scoped css="..."> css prop should have the '&' character in it to scope the css classes`);
+    }
+
+    let styleRef, kremlingAttrName, kremlingAttrValue;
+    const existingDomEl = styleTags[props.css];
+
+    if (existingDomEl) {
+      styleRef = existingDomEl;
+      existingDomEl.kremlings++;
+      kremlingAttrName = styleRef.kremlingAttr;
+      kremlingAttrValue = styleRef.kremlingValue;
+    } else {
+      // The attribute for namespacing the css
+      kremlingAttrName = `data-${props.namespace || Scoped.defaultNamespace}`;
+      kremlingAttrValue = counter++;
+
+      // The css to append to the dom
+      const kremlingSelector = `[${kremlingAttrName}="${kremlingAttrValue}"]`;
+      const transformedCSS = props.css.replace(/& (.+){/g, (match, cssRule) => {
+        cssRule = cssRule.trim();
+        return `${kremlingSelector} ${cssRule}, ${kremlingSelector}${cssRule} {`;
+      });
+
+      // The dom element
+      const el = document.createElement('style');
+      el.setAttribute('type', 'text/css');
+      el.textContent = transformedCSS;
+      el.kremlings = 1;
+      el.kremlingAttr = kremlingSelector;
+      el.kremlingValue = kremlingAttrValue;
+      document.head.appendChild(el);
+      styleTags[props.css] = el;
+      styleRef = el;
+    }
+
+    return {
+      css: props.css,
+      styleRef,
+      kremlingAttrName,
+      kremlingAttrValue,
+    };
+  }
+
+  doneWithCss = () => {
+    if (this.state.styleRef && --this.state.styleRef.kremlings === 0) {
+      this.state.styleRef.parentNode.removeChild(this.state.styleRef);
       delete styleTags[this.props.css];
     }
   }
