f262f11937bfc940f4e064f21fbb7a66f2115511
Joel Denning <joel.denning@canopytax.com>
2018-02-09 10:30:20 -0700

    More fixing chrome headless tests

--- karma.conf.js
+++ karma.conf.js
@@ -76,6 +76,13 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ["ChromeHeadlessNoSandbox"],
 
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
