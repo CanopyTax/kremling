ff12ebc3645b7bdfc3f02490823275883d83d3f6
Joel Denning <joeldenning@gmail.com>
2019-02-03 18:52:12 -0700

    Implementing useCss hook.

---
+++ src/use-css.hook.js
@@ -0,0 +1,48 @@
+import {useEffect, useState} from 'react'
+import {Scoped} from './scoped.component.js'
+import {styleTags, incrementCounter, getCurrentCounter, transformCss} from './style-element-utils.js'
+
+export function useCss(css, overrideNamespace) {
+  const [kremlingId, setKremlingId] = useState(getCurrentCounter())
+  const isPostCss = css && css.id && css.styles
+  const namespace = overrideNamespace || (isPostCss && isPostCss.namespace) || Scoped.defaultNamespace
+  const kremlingAttrName = isPostCss ? namespace : `data-${namespace}`
+
+  useStyleElement()
+
+  return {
+    [kremlingAttrName]: String(kremlingId).toString(),
+  }
+
+  function useStyleElement() {
+    useEffect(() => {
+      let styleElement = isPostCss ? document.head.querySelector(`[${kremlingAttrName}='${kremlingId}']`) : styleTags[css]
+
+      if (!styleElement) {
+        const newKremlingId = isPostCss ? css.id : incrementCounter()
+        const kremlingSelector = `[${kremlingAttrName}='${newKremlingId}']`
+        const rawCss = isPostCss ? css.styles : css
+        const cssToInsert = isPostCss ? css.styles : transformCss(css, kremlingSelector)
+
+        styleElement = document.createElement('style')
+        styleElement.type = 'text/css'
+        styleElement.textContent = cssToInsert
+        styleElement.kremlings = 1
+        styleElement.kremlingAttr = kremlingAttrName
+        styleElement.kremlingValue = newKremlingId
+        document.head.appendChild(styleElement)
+
+        styleTags[rawCss] = styleElement
+
+        setKremlingId(newKremlingId)
+
+        return () => {
+          if (--styleElement.kremlings === 0) {
+            document.head.removeChild(styleElement)
+            delete styleTags[css]
+          }
+        }
+      }
+    }, [css, namespace, kremlingAttrName])
+  }
+}
