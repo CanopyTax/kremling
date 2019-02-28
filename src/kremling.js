d41145994f95da60b6b858b319f94a426eb2ee88
tonylefler <tonylefler@gmail.com>
2019-02-28 15:27:27 -0700

    move aliases into classname helper file

--- src/kremling.js
+++ src/kremling.js
@@ -1,3 +1,3 @@
-export {always, always as a, maybe, maybe as m, toggle, toggle as t, css, css as c} from './classname-helpers.js';
+export {always, a, maybe, m, toggle, t, css, c} from './classname-helpers.js';
 export {Scoped} from './scoped.component.js';
 export {useCss} from './use-css.hook.js';
