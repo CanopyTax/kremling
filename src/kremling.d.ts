98ffb99a43e13586de9429fb677df66393cc70ab
geoctrl <tonylefler@gmail.com>
2021-12-14 23:59:33 -0700

    handle variable number of arguments for "always"

--- src/kremling.d.ts
+++ src/kremling.d.ts
@@ -4,8 +4,8 @@ export = Kremling;
 
 declare namespace Kremling {
   function useCss(css: string | object): Scope;
-  function always(className: string): KremlingString & string;
-  function a(className: string): KremlingString & string;
+  function always(...className: string[]): KremlingString & string;
+  function a(...className: string[]): KremlingString & string;
   function maybe(className: string, condition: any): KremlingString & string;
   function m(className: string, condition: any): KremlingString & string;
   function toggle(truthyClass: string, falsyClass: string, condition: any): KremlingString & string;
@@ -13,8 +13,8 @@ declare namespace Kremling {
   function k(strings: TemplateStringsArray, ...args: Array<string>): object | string;
 
   interface KremlingString extends String {
-    always(className: string): KremlingString & string,
-    a(className: string): KremlingString & string,
+    always(...className: string[]): KremlingString & string,
+    a(...className: string[]): KremlingString & string,
     maybe(className: string, condition: any): KremlingString & string,
     m(className: string, condition: any): KremlingString & string,
     toggle(truthyClass: string, falsyClass: string, condition: any): KremlingString & string,
